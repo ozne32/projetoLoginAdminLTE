3aba1e48141cbcb77053f3a4c829c7de6706a4db
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-08 14:06:57 -0300

    update de routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/login', function(){
     return view('login');
 });
 Route::get('/sign-in', function(){
